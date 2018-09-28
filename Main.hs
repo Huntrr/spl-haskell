a96ae7b319fcc2c989569c283a6ecf330af8882b
Hunter Lightman <git.huntrr@gmail.com>
2018-09-28 12:32:21 -0400

    Revert "Parsing"

--- Main.hs
+++ Main.hs
@@ -4,26 +4,6 @@
 
 module Main where
 -- | provides command line interface for interpreting SPL code
-import LanguageParser
-import Evaluator
-
-import System.Environment (getArgs)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    file:_ -> runFile file
-    _        -> error "Proper usage: spl filename"
-
-runFile :: String -> IO ()
-runFile file = do p <- parseFile file
-                  runIO p
-
-runFileInts :: String -> [Int] -> IO ()
-runFileInts file input = do p <- parseFile file
-                            putStr $ runInt p input
-
-runFileString :: String -> String -> IO ()
-runFileString file input = do p <- parseFile file
-                              putStr $ runString p input
+main = undefined
